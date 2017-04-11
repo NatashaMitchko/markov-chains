73b7a6406f7ec237688624d254b9882c2bc371e7
Hackbright Student <no-reply@hackbrightacademy.com>
2017-04-11 12:22:47 -0700

    Wrote functions for open_and_read_file and make_chains to create our dictionary

---
+++ markov.py
@@ -0,0 +1,84 @@
+"""Generate markov text from text files."""
+
+
+from random import choice
+
+
+def open_and_read_file(file_path):
+    """Takes file path as string; returns text as string.
+
+    Takes a string that is a file path, opens the file, and turns
+    the file's contents as one string of text.
+    """
+
+    contents = open(file_path).read()
+    # print contents
+    return contents
+
+contents = open_and_read_file("green-eggs.txt")
+
+
+def make_chains(text_string):
+    """Takes input text as string; returns dictionary of markov chains.
+
+    A chain will be a key that consists of a tuple of (word1, word2)
+    and the value would be a list of the word(s) that follow those two
+    words in the input text.
+
+    For example:
+
+        >>> chains = make_chains("hi there mary hi there juanita")
+
+    Each bigram (except the last) will be a key in chains:
+
+        >>> sorted(chains.keys())
+        [('hi', 'there'), ('mary', 'hi'), ('there', 'mary')]
+
+    Each item in chains is a list of all possible following words:
+
+        >>> chains[('hi', 'there')]
+        ['mary', 'juanita']
+    """
+
+    chains = {}
+
+    words = text_string.split()
+    for i in range(len(words) - 2):
+
+        bigram = (words[i], words[i + 1])
+        next_word = words[i + 2]
+
+        # If key does not exist in dictionary, add the (key, value) pair
+        # to chains.
+        if bigram not in chains:
+            chains[bigram] = [next_word]
+        else:
+            chains[bigram].append(next_word)
+
+    return chains
+
+print make_chains(contents)
+
+
+def make_text(chains):
+    """Returns text from chains."""
+
+    words = []
+
+    # your code goes here
+
+    return " ".join(words)
+
+
+input_path = "green-eggs.txt"
+
+# Open the file and turn it into one long string
+input_text = open_and_read_file(input_path)
+
+# Get a Markov chain
+chains = make_chains(input_text)
+
+# Produce random text
+random_text = make_text(chains)
+
+print random_text
